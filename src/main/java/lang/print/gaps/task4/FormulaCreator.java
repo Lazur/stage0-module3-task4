fb19c9d3bf21e6ef085616520df57772516f6c46
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-05-28 15:56:07 +0300

    Module 3 task 4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a;
+        double b;
+        a=3.12;
+        b=3.42;
+
+        double formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(formula);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+
+        int charC = (char) c;
+        int charH = (char) h;
+        int charA = (char) a;
+        int charR = (char) r;
+        
+        System.out.println(charC);
+        System.out.println(charH);
+        System.out.println(charA);
+        System.out.println(charR);
     }
 }
